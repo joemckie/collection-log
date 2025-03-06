3304592b7c2c37c42464886fa2bc65972aaf706f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-06 19:34:27 +0000

    feat: add prettier

--- src/app/api/collection-log/[user]/route.ts
+++ src/app/api/collection-log/[user]/route.ts
@@ -1,9 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
-import collectionLogFixture from "@/mocks/collection-log/cousinofkos.json";
+import { NextRequest, NextResponse } from 'next/server';
+import collectionLogFixture from '@/mocks/collection-log/cousinofkos.json';
 
-export async function GET(_request: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
-  console.log(await params)
-  
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: Promise<{ slug: string }> },
+) {
+  console.log(await params);
+
   return NextResponse.json(collectionLogFixture);
 }
 
