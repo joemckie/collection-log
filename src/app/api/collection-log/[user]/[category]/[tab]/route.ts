bf119ab792e87afa7f776f9e8f30214980dfa72f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-07 10:22:43 +0000

    chore: configure prettier

--- src/app/api/collection-log/[user]/[category]/[tab]/route.ts
+++ src/app/api/collection-log/[user]/[category]/[tab]/route.ts
@@ -1,13 +1,20 @@
-import { redis } from "@/redis";
-import { CollectionLogTabContents } from "@/schemas/collection-log.schema";
-import { NextRequest, NextResponse } from "next/server";
+import { redis } from '@/redis';
+import { CollectionLogTabContents } from '@/schemas/collection-log.schema';
+import { NextRequest, NextResponse } from 'next/server';
 
 type Params = Promise<{ user: string; category: string; tab: string }>;
 
-export async function GET(_request: NextRequest, { params }: { params: Params }) {
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: Params },
+) {
   const { category, tab, user } = await params;
 
-  const data = await redis.json.get<[CollectionLogTabContents]>(`collection-log:${user}`, {}, `$.tabs["${category}"]["${tab}"]`);
+  const data = await redis.json.get<[CollectionLogTabContents]>(
+    `collection-log:${user}`,
+    {},
+    `$.tabs["${category}"]["${tab}"]`,
+  );
 
   return NextResponse.json(data?.[0]);
 }
