80ce90879076176a9df59eadfb9f05c65da54eb5
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-07 00:31:45 +0000

    feat: add single tab get handler

---
+++ src/app/api/collection-log/[user]/[category]/[tab]/route.ts
@@ -0,0 +1,12 @@
+import { redis } from "@/redis";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = Promise<{ user: string; category: string; tab: string }>;
+
+export async function GET(_request: NextRequest, { params }: { params: Params }) {
+  const { category, tab, user } = await params;
+
+  const data = await redis.json.get(`collection-log:${user}`, {}, `$.tabs.${category}.${tab}`);
+
+  return NextResponse.json(data)
+}
