7e7e8004f3c89583b58b58a0c00e339d9b2ce8de
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-07 00:34:23 +0000

    fix: spaces in json path

--- src/app/api/collection-log/[user]/[category]/[tab]/route.ts
+++ src/app/api/collection-log/[user]/[category]/[tab]/route.ts
@@ -6,7 +6,7 @@ type Params = Promise<{ user: string; category: string; tab: string }>;
 export async function GET(_request: NextRequest, { params }: { params: Params }) {
   const { category, tab, user } = await params;
 
-  const data = await redis.json.get(`collection-log:${user}`, {}, `$.tabs.${category}.${tab}`);
+  const data = await redis.json.get(`collection-log:${user}`, {}, `$.tabs["${category}"]["${tab}"]`);
 
   return NextResponse.json(data)
 }
